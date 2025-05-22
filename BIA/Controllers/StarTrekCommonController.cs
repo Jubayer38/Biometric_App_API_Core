886c80f1519dba3bc9e8981f990eecacfb03d81e
jr.hossain <jr.hossain@blmanagedservices.com>
2025-05-22 12:36:28 +0600

    CWE-561 is not solved so tried different approch again

--- BIA/Controllers/StarTrekCommonController.cs
+++ BIA/Controllers/StarTrekCommonController.cs
@@ -4117,9 +4117,16 @@ namespace BIA.Controllers
                 else if (dbssResp?["data"]?["status"]?.ToString().ToLower() == "failed")
                 {
                     response.result = false;
-                    response.message = string.IsNullOrEmpty(dbssResp?["data"]?["error_message"]?.ToString())
-                ? MessageCollection.SIMIsNotInInventory
-                : dbssResp?["data"]?["error_message"]?.ToString();
+                    
+                    if(string.IsNullOrEmpty(dbssResp?["data"]?["error_message"]?.ToString()))
+                    {
+                        response.message = MessageCollection.SIMIsNotInInventory;
+                    }
+                    else
+                    {
+                        response.message = dbssResp?["data"]?["error_message"]?.ToString();
+                    }
+
                     return response;
                 }
                 else if (dbssResp?["data"]?["logical_inventory_status"]?.ToString().ToLower() == "used")
